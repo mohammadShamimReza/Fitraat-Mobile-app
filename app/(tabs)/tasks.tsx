b3a105fe8869a940bff97796b6704b6549e90477
mohammadshamimreza <mohammadshamimreza23393@gmail.com>
2024-08-03 21:53:12 +0600

    initial commit

---
+++ app/(tabs)/tasks.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+const tasks = () => {
+  return (
+    <View>
+      <Text>tasks</Text>
+    </View>
+  );
+};
+
+export default tasks;
+
+const styles = StyleSheet.create({});
