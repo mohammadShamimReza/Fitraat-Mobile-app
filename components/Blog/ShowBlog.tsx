e78d51aedd0d586be32e93151020dde22e9f58f5
mohammadshamimreza <mohammadshamimreza23393@gmail.com>
2024-12-11 20:50:38 +0600

    add marked doen

--- components/Blog/ShowBlog.tsx
+++ components/Blog/ShowBlog.tsx
@@ -1,7 +1,8 @@
 import { Blog } from "@/types/contantType";
 import { Link } from "expo-router";
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, TextStyle, View } from "react-native";
+import Markdown from "react-native-markdown-display";
 
 // Define the prop types for the Blogs component
 interface BlogsProps {
@@ -17,23 +18,19 @@ const ShowBlog: React.FC<BlogsProps> = ({ blog }) => {
       <Link href={`/freeBlogs/${blog.id}`} style={styles.blogLink}>
         <Text style={styles.blogTitle}>{blogData.title}</Text>
       </Link>
-      <Link href={`/freeBlogs/${blog.id}`} style={styles.blogLink}>
-        <Text style={styles.blogDescription}>
-          {blogData?.content && (
-            <Text style={styles.blogContentText}>
-              {blogData.content.slice(0, 200)}...{" "}
-              {/* Show a preview of 100 characters */}
-            </Text>
-          )}
-        </Text>
-      </Link>
+      <View style={styles.blogContent}>
+        {blogData?.content && (
+          <Markdown style={markdownStyles}>
+            {blogData.content.slice(0, 200) + "..."}
+          </Markdown>
+        )}
+      </View>
       <View style={styles.blogFooter}>
         <Text style={styles.blogUpdatedAt}>
           Updated At:{" "}
           <Text style={styles.blogUpdatedAtDate}>{blogUpdatedAt}</Text>
         </Text>
         <View style={styles.blogAuthor}>
-          {/* <Image source={myPic} style={styles.authorImage} /> */}
           <Text style={styles.authorName}>
             {blogData?.authorName.split(" ").slice(0, 2).join(" ")}
           </Text>
@@ -48,7 +45,7 @@ const styles = StyleSheet.create({
     marginHorizontal: 10,
     marginTop: 20,
     marginBottom: 20,
-    backgroundColor: "#f9f9f9", // Light background for contrast
+    backgroundColor: "#f9f9f9",
     borderRadius: 8,
     padding: 16,
     shadowColor: "#000",
@@ -66,15 +63,8 @@ const styles = StyleSheet.create({
     color: "#000",
     marginBottom: 8,
   },
-  blogDescription: {
+  blogContent: {
     marginVertical: 10,
-    fontSize: 16,
-    color: "#333",
-    lineHeight: 22,
-  },
-  blogContentText: {
-    fontSize: 16,
-    color: "#555",
   },
   blogFooter: {
     flexDirection: "row",
@@ -98,17 +88,28 @@ const styles = StyleSheet.create({
     flexDirection: "row",
     alignItems: "center",
   },
-  authorImage: {
-    width: 30,
-    height: 30,
-    borderRadius: 15,
-    marginRight: 8,
-  },
   authorName: {
     fontSize: 14,
     color: "#333",
     fontWeight: "bold",
   },
 });
+
+const markdownStyles: { [key: string]: TextStyle } = {
+  body: {
+    fontSize: 16,
+    color: "#555",
+    lineHeight: 22,
+  },
+  heading1: {
+    fontSize: 20,
+    fontWeight: "bold" as TextStyle["fontWeight"], // Explicit type for fontWeight
+    color: "#000",
+  },
+  link: {
+    color: "#1e90ff",
+  },
+};
+
 
 export default ShowBlog;
